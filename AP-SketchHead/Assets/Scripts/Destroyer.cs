78618f9415bc17fec3d7732cfe8967608bf57a97
rebecca <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-04 17:58:20 -0500

    Step 34

--- AP-SketchHead/Assets/Scripts/Destroyer.cs
+++ AP-SketchHead/Assets/Scripts/Destroyer.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Find("DoodleHead").SetActive(false);
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+    }
     // Start is called before the first frame update
     void Start()
     {
--- AP-SketchHead/Assets/Scripts/GameController.cs
+++ AP-SketchHead/Assets/Scripts/GameController.cs
@@ -5,15 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Platform gameobject
+    [Header("Platform Object")]
+    public GameObject platform;
+    //Default position for platform
+    float pos = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Interger i equals 1000
+        for (int i = 0; i < 1000; i++)
+        {
+            //Execute SpawnPlatforms
+            SpawnPlatforms();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    //Spawn platforms function
+    void SpawnPlatforms()
+    {
+        //Spawn platforms randomly on the x axis and place them on the Y axis every 2.5
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+    //Game Over Canvas
+    [Header("Game Over UI Canvas Object")]
+    public GameObject gameOverCanvas;
+
+    //Game over function
+    public void GameOver()
+    {
+        //Game Over Canvas is active
+        gameOverCanvas.SetActive(true);
     }
 }
