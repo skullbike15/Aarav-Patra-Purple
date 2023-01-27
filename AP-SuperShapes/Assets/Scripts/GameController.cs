40c1c687bf16d1d23cc56ee4e08432b226e5aeb1
Chelsea <102189845+hellomynameisaj@users.noreply.github.com>
2023-01-26 19:58:59 -0500

    step 34

--- AP-SuperShapes/Assets/Scripts/GameController.cs
+++ AP-SuperShapes/Assets/Scripts/GameController.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Hexagon game object
+    [Header("Shape Objects")]
+    public GameObject[] shapePrefabs;
+    //Spawn rate for how many
+    //objects are spawned
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 2f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
 
+    void Spawn()
+    {
+        //Random range for shape objects to be spawned
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        //Spawn new hexagom at position
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -17,3 +34,5 @@ public class GameController : MonoBehaviour
 
     }
 }
+
+
