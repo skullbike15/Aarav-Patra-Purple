099fb55bfc2bd75c7dfc41ca312c34fbb3fa720f
panda <pandas.are.cool@protonmail.com>
2023-03-15 19:02:27 -0400

    done

--- AP-Stack/Assets/Scripts/GameController.cs
+++ AP-Stack/Assets/Scripts/GameController.cs
@@ -110,18 +110,20 @@ public class GameController : MonoBehaviour
         var pos1 = lastCube.transform.position + Vector3.up * 10f;
         //Variable pos2 equals to the pos1 plus any level by number of 2
         var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        //Variable pos3 equals to Lerp
+        var pos3 = pos1 + ((Level % 2 == 0) ? Vector3.right : Vector3.back) * 120;
         //If the level is by the number of two
         if (Level % 2 == 0)
         {
             //Current position of the current cube based of the 3 axis of
             //pos2, pos1, and time
-            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+            currentCube.transform.position = Vector3.Lerp(pos2, pos3, time);
         }
         else
         {
             //Current position of the current cube based of the 3 axis of
             //pos1, pos2, and time
-            currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+            currentCube.transform.position = Vector3.Lerp(pos3, pos2, time);
         }
         //If left mouse button is clicked
         if (Input.GetMouseButtonDown(0))
@@ -129,6 +131,9 @@ public class GameController : MonoBehaviour
             //New block function
             //is called
             Newblock();
+            //Text equals to the text of the Final score
+            //and which level is played
+            text.text = "Score: " + Level;
         }
     }
 
